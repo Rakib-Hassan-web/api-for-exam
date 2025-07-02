596881de4b79dc7e38200c04376f1dccb2da4f48
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-07-03 00:56:47 +0600

    .then and map method

--- js/main.js
+++ js/main.js
@@ -9,6 +9,27 @@ let input =document.querySelector('.input')
 
 
 fetch('https://jsonplaceholder.typicode.com/users')
+.then( res => res .json())
+.then ( data =>{
+
+
+    data.map( user =>{
+
+
+        let main_id =do
+
+
+
+
+    })
+
+        console.log(data)
+
+
+
+
+
+})
 
 
 
